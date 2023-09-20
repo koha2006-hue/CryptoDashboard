74159698cdd1cd228a77f60aceb32f57b37258d4
koha2006-hue <khoadh.bi12-215@st.usth.edu.vn>
2023-09-20 16:28:31 +0700

    update icon, delete unused files

--- app/src/main/java/com/example/mycrypto/SignupFragment.java
+++ app/src/main/java/com/example/mycrypto/SignupFragment.java
@@ -1,6 +1,6 @@
 package com.example.mycrypto;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
